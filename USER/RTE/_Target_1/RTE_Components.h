d4469d967e297437160f5baa722c6f9da18179ef
havenxie <1428876581@qq.com>
2019-05-05 12:15:34 +0800

    rename project

--- USER/RTE/_Target_1/RTE_Components.h
+++ USER/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'UsbCardReader' 
+ * Project: 'IAP-MSD' 
  * Target:  'Target 1' 
  */
 
